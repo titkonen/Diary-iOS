4c1f1322cf8d2e2deb12b49c8af72f5a1109e6c3
Toni Itkonen <toni.itkonen@icloud.com>
2021-08-19 14:08:20 +0300

    tableCell, subtitle added

--- Diary/DogWalk/CoreDataStack.swift
+++ Diary/DogWalk/CoreDataStack.swift
@@ -1,9 +1,36 @@
-//
-//  CoreDataStack.swift
-//  DogWalk
-//
-//  Created by Toni Itkonen on 19.8.2021.
-//  Copyright © 2021 Razeware. All rights reserved.
-//
-
 import Foundation
+import CoreData
+
+class CoreDataStack {
+  private let modelName: String
+  
+  lazy var managedContext: NSManagedObjectContext = {
+    return self.storeContainer.viewContext
+  }()
+  
+  init(modelName: String) {
+    self.modelName = modelName
+  }
+  
+  private lazy var storeContainer: NSPersistentContainer = {
+
+    let container = NSPersistentContainer(name: self.modelName)
+    container.loadPersistentStores { _, error in
+      if let error = error as NSError? {
+        print("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    return container
+  }()
+  
+  func saveContext () {
+    guard managedContext.hasChanges else { return }
+    
+    do {
+      try managedContext.save()
+    } catch let error as NSError {
+      print("Unresolved error \(error), \(error.userInfo)")
+    }
+  }
+  
+}
--- Diary/Paivays+CoreDataProperties.swift
+++ Diary/Paivays+CoreDataProperties.swift
@@ -1,12 +1,3 @@
-//
-//  Paivays+CoreDataProperties.swift
-//  DogWalk
-//
-//  Created by Toni Itkonen on 19.8.2021.
-//  Copyright © 2021 Razeware. All rights reserved.
-//
-//
-
 import Foundation
 import CoreData
 
